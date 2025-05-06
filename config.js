f1cfb16c3debfe2b79a5c3ee9e856c3a58be263e
DaviLealdev <198823429+DaviLealdev@users.noreply.github.com>
2025-05-06 12:59:57 -0300

    save config

--- config.js
+++ config.js
@@ -3,13 +3,15 @@ const {app, BrowserWindow} = require('electron');
 let mainWindow;
 
 app.on('ready', () =>{
-
+    
     mainWindow = new BrowserWindow({
-        center: true,
-        fullscreen:true,
-        resizable:true
+        show: false,
+        frame:true
 
     })
+    mainWindow.maximize();
+    mainWindow.show()
+    mainWindow.removeMenu();
 
     mainWindow.loadURL(`file://${__dirname}/index.html`)
 
